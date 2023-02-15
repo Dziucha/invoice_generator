c73a031dbbc9800589a5fe9e45829912ab34af06
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-15 16:01:02 +0100

    creates empty pdf file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
 
 filepaths = glob.glob("excel_files/*xlsx")
 
 for filepath in filepaths:
     excel_data = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(excel_data)
+    pdf = FPDF(orientation="P", format="A4", unit="mm")
+
+    file_name = filepath.strip(".xlsx")
+    file_name = file_name.lstrip("excel_files/")
+    pdf.output(f"pdf_invoices/{file_name}.pdf")
