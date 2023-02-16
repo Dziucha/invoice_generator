364691ada384880bb6447de61a34015668983ea2
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-16 16:47:38 +0100

    information from file name updated

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from pathlib import Path
 filepaths = glob.glob("excel_files/*xlsx")
 
 for filepath in filepaths:
-    excel_data = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     file_name = Path(filepath).stem
 
     pdf = FPDF(orientation="P", format="A4", unit="mm")
@@ -16,15 +14,16 @@ for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
 
-    file_name_data = file_name.split(sep="-")
-    invoice_nr = file_name_data[0]
+    invoice_nr, date_of_invoice = file_name.split(sep="-")
+
     pdf.cell(w=0, h=10, txt=f"Invoice nr. {invoice_nr}",
              align="L", border=1, ln=1)
-    date_of_invoice = file_name_data[1]
     pdf.cell(w=0, h=10, txt=f"Date {date_of_invoice}",
              align="L", border=1, ln=1)
 
-    total_price = 0
+    excel_data = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+
     total_price = excel_data["total_price"].sum()
 
     pdf.set_font(family="Times", size=10, style="B")
